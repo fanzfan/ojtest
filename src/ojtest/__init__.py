658988fc1f31852e43e4cef2d348da88a2c4fb8e
VelaQ <71018134+VelaQ@users.noreply.github.com>
2022-11-12 10:43:09 +0800

    update init.py

--- src/ojtest/__init__.py
+++ src/ojtest/__init__.py
@@ -1,10 +1,13 @@
-from ojtest import rand_list, rand_str
+from ojtest import rand_list as rl, rand_str as rs
 
-rand_int_list = rand_list.rand_int_list
-rand_int_list_unique = rand_list.rand_int_list_unique
-rand_int_list_sorted = rand_list.rand_int_list_sorted
-rand_int_list_sorted_unique = rand_list.rand_int_list_sorted_unique
-rand_str_alphabetic_lower = rand_str.rand_str_alphabetic_lower
-rand_str_alphabetic_capital = rand_str.rand_str_alphabetic_capital
-rand_str_alphabetic = rand_str.rand_str_alphabetic
-rand_str_digital = rand_str.rand_str_digital
+rand_int_list = rl.rand_int_list
+rand_int_list_unique = rl.rand_int_list_unique
+rand_int_list_sorted = rl.rand_int_list_sorted
+rand_int_list_sorted_unique = rl.rand_int_list_sorted_unique
+
+
+rand_str = rs.rand_str
+rand_str_alphabetic_lower = rs.rand_str_alphabetic_lower
+rand_str_alphabetic_capital = rs.rand_str_alphabetic_capital
+rand_str_alphabetic = rs.rand_str_alphabetic
+rand_str_digital = rs.rand_str_digital
